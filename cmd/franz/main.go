f3d78b6e69d10e0b79c1c80a794a975735dcf1c2
Alain <alain.devcs@gmail.com>
2023-08-06 16:26:07 +0100

    Moving to goroutines for each bit

--- cmd/franz/main.go
+++ cmd/franz/main.go
@@ -5,50 +5,48 @@ import (
 	"franz/internal/admin"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	log "github.com/sirupsen/logrus"
 )
 
 var broker = "localhost:29092"
+var RefreshTopicDuration = 5 * time.Second
 
-func main() {
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+type AvailableTopics struct {
+	mutex  sync.Mutex
+	Topics []string
+}
 
-	log.SetFormatter(&log.JSONFormatter{})
-	l := os.Getenv("LOG_TEXT")
-	if l != "" {
-		log.SetFormatter(&log.TextFormatter{})
-	}
-
-	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": broker,
-		"group.id":          "franz",
-		"auto.offset.reset": "earliest",
-	})
-	if err != nil {
-		log.Errorf("Failed to create consumer: %v", err)
-		return
-	}
-	defer c.Close()
-
-	adminClient, err := admin.NewAdminClient(c)
-	if err != nil {
-		log.Errorf("Failed to create admin client: %v", err)
-		return
-	}
-
-	topics, err := adminClient.GetTopics(false)
+func updateTopics(c admin.KafkaAdminer, availableTopics *AvailableTopics) {
+	topics, err := c.GetTopics(false)
 	if err != nil {
 		log.Errorf("Failed to get available topics: %v", err)
 		return
 	}
-
 	log.Infof("Auto-discovered topics: %v", topics)
+	availableTopics.mutex.Lock()
+	availableTopics.Topics = topics
+	availableTopics.mutex.Unlock()
+}
 
-	err = c.SubscribeTopics(topics, nil)
+func RefreshTopicsRoutine(c admin.KafkaAdminer, availableTopics *AvailableTopics, quit chan struct{}) {
+	updateTopics(c, availableTopics)
+	for {
+		select {
+		case <-time.After(RefreshTopicDuration):
+			updateTopics(c, availableTopics)
+		case <-quit:
+			return
+		}
+	}
+}
+
+func ScrapeRoutine(c *kafka.Consumer, availableTopics *AvailableTopics, quit chan struct{}) {
+	err := c.SubscribeTopics(availableTopics.Topics, nil)
 	if err != nil {
 		log.Errorf("Failed to subscribe to topics: %v", err)
 		return
@@ -59,8 +57,11 @@ func main() {
 	run := true
 	for run {
 		select {
-		case sig := <-sigchan:
-			log.Infof("Caught signal %v: terminating", sig)
+		case <-quit:
+			err := c.Close()
+			if err != nil {
+				log.Errorf("Failed to close consumer: %v", err)
+			}
 			run = false
 		default:
 			ev := c.Poll(100)
@@ -92,5 +93,57 @@ func main() {
 				log.Errorf("Error: %v", e)
 			}
 		}
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	availableTopics := &AvailableTopics{}
+	quit := make(chan struct{})
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	log.SetFormatter(&log.JSONFormatter{})
+	l := os.Getenv("LOG_TEXT")
+	if l != "" {
+		log.SetFormatter(&log.TextFormatter{})
+	}
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": broker,
+		"group.id":          "franz",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		log.Errorf("Failed to create consumer: %v", err)
+		return
+	}
+	defer c.Close()
+
+	adminClient, err := admin.NewAdminClient(c)
+	if err != nil {
+		log.Errorf("Failed to create admin client: %v", err)
+		return
+	}
+
+	updateTopics(adminClient, availableTopics)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		RefreshTopicsRoutine(adminClient, availableTopics, quit)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ScrapeRoutine(c, availableTopics, quit)
+	}()
+
+	select {
+	case sig := <-sigchan:
+		log.Infof("Caught signal %v: terminating", sig)
+		close(quit)
 	}
 }
